d720d293a8ef42533c53284fc79a8ba9bfee30b9
Horatio <horatiohab@gmail.com>
2026-04-02 22:35:30 +0100

    add card flip reveal animation

--- components/clue-cards/clue-cards.component.js
+++ components/clue-cards/clue-cards.component.js
@@ -9,16 +9,37 @@ class ClueCardsComponent extends Elemental {
             <span><bind>first</bind></span>
         </div>
 
-        <div class="clue-cards__card clue-cards__card--second" data-class="{ 'clue-cards__card--locked': unlockClue1 }">
-            <span><bind>second</bind></span>
+        <div class="clue-cards__card-container clue-cards__card-container--second">
+            <div class="clue-cards__card-flipper" data-class="{ 'clue-cards__card-flipper--revealed': revealClue1, 'clue-cards__card-flipper--reveal': revealClue1 && !revealClue2 }">
+                <div class="clue-cards__card-face clue-cards__card-face--front">
+                    <span><bind>second</bind></span>
+                </div>
+                <div class="clue-cards__card-face clue-cards__card-face--back">
+                    <span></span>
+                </div>
+            </div>
         </div>
 
-        <div class="clue-cards__card clue-cards__card--third" data-class="{ 'clue-cards__card--locked': unlockClue2 }">
-            <span><bind>third</bind></span>
+        <div class="clue-cards__card-container clue-cards__card-container--third">
+            <div class="clue-cards__card-flipper" data-class="{ 'clue-cards__card-flipper--revealed': revealClue2, 'clue-cards__card-flipper--reveal': revealClue2 && !revealClue3 }">
+                <div class="clue-cards__card-face clue-cards__card-face--front">
+                    <span><bind>third</bind></span>
+                </div>
+                <div class="clue-cards__card-face clue-cards__card-face--back">
+                    <span></span>
+                </div>
+            </div>
         </div>
 
-        <div class="clue-cards__card clue-cards__card--fourth" data-class="{ 'clue-cards__card--locked': unlockClue3 }">
-            <span><bind>fourth</bind></span>
+        <div class="clue-cards__card-container clue-cards__card-container--fourth">
+            <div class="clue-cards__card-flipper" data-class="{ 'clue-cards__card-flipper--revealed': revealClue3, 'clue-cards__card-flipper--reveal': revealClue3 }">
+                <div class="clue-cards__card-face clue-cards__card-face--front">
+                    <span><bind>fourth</bind></span>
+                </div>
+                <div class="clue-cards__card-face clue-cards__card-face--back">
+                    <span></span>
+                </div>
+            </div>
         </div>
     `;
 
@@ -29,9 +50,9 @@ class ClueCardsComponent extends Elemental {
             second: '',
             third: '',
             fourth: '',
-            unlockClue1: true,
-            unlockClue2: true,
-            unlockClue3: true,
+            revealClue1: false,
+            revealClue2: false,
+            revealClue3: false,
         };
     }
 
@@ -43,9 +64,9 @@ class ClueCardsComponent extends Elemental {
             second: clueCards.second || '',
             third: clueCards.third || '',
             fourth: clueCards.fourth || '',
-            unlockClue1: roundService.guesses < 1,
-            unlockClue2: roundService.guesses < 2,
-            unlockClue3: roundService.guesses < 3,
+            revealClue1: roundService.guesses >= 1,
+            revealClue2: roundService.guesses >= 2,
+            revealClue3: roundService.guesses >= 3,
         };
     }
 
@@ -71,11 +92,11 @@ class ClueCardsComponent extends Elemental {
             width: 100%;
         }
 
-        .clue-cards__card {
+        .clue-cards__card,
+        .clue-cards__card-face {
             display: flex;
             justify-content: center;
             align-items: center;
-            margin: 10px 0;
             height: 60px;
             box-shadow: var(--shadow-soft);
             border-radius: 12px;
@@ -87,26 +108,75 @@ class ClueCardsComponent extends Elemental {
         }
 
         .clue-cards__card--first {
+            width: 100%;
+            margin: 10px 0;
             background: var(--clue-first-bg);
         }
 
-        .clue-cards__card--second {
-            background: var(--clue-second-bg);
+        .clue-cards__card-container {
+            height: 60px;
+            width: 100%;
+            perspective: 500px;
+            margin: 10px 0;
         }
 
-        .clue-cards__card--third {
-            background: var(--clue-third-bg);
+        .clue-cards__card-flipper {
+            width: 100%;
+            height: 100%;
+            position: relative;
+            transition: transform 0.5s ease;
+            transform-style: preserve-3d;
+            transform: rotateX(0deg);
         }
 
-        .clue-cards__card--fourth {
-            background: var(--clue-fourth-bg);
+        .clue-cards__card-face {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            backface-visibility: hidden;
         }
 
-        .clue-cards__card--locked {
+        .clue-cards__card-face--front {
+            transform: rotateX(180deg);
+        }
+
+        .clue-cards__card-face--back {
             opacity: 0.3;
             filter: saturate(0.7);
             box-shadow: none;
         }
+
+        .clue-cards__card-flipper--revealed {
+            transform: rotateX(180deg);
+        }
+
+        .clue-cards__card-flipper--reveal {
+            animation: clue-cards-flip-reveal 0.5s ease forwards;
+        }
+
+        @keyframes clue-cards-flip-reveal {
+            from {
+                transform: rotateX(0deg);
+            }
+            to {
+                transform: rotateX(180deg);
+            }
+        }
+
+        .clue-cards__card-container--second .clue-cards__card-face {
+            background: var(--clue-second-bg);
+        }
+
+        .clue-cards__card-container--third .clue-cards__card-face {
+            background: var(--clue-third-bg);
+        }
+
+        .clue-cards__card-container--fourth .clue-cards__card-face {
+            background: var(--clue-fourth-bg);
+        }
+
     `;
 }
 
